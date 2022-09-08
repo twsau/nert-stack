1d3173b2a56e311fc741297f723a1e915c7a6070
twsau <contact.twsau@gmail.com>
2022-09-08 09:23:27 +0100

    improve footer

--- client/src/core/layout/_Footer.tsx
+++ client/src/core/layout/_Footer.tsx
@@ -2,11 +2,7 @@ import styled from "styled-components";
 
 export const Footer = styled.footer`
   background-color: var(--surface-2);
-  bottom: 0;
-  box-shadow: var(--shadow-2);
   display: flex;
   justify-content: center;
   padding: var(--size-3);
-  position: absolute;
-  width: 100%;
 `;
